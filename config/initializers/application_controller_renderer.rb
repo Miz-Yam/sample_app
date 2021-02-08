f02fdf8915b452690617a54a9ff0c8f3f0df5847
Miz-Yam <gyakusyuboo@gmail.com>
2021-02-08 04:33:51 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c567b7f974889c6669dc953829eba3caef614cf81ccd42b0e4edd3d94a05219a7b663b48330428d26642186a0357b60b587c1d99abbf58e74e78574a79a5e70'
